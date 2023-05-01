de582d17f2157cd862ef68e387077e953d2cc883
XuCai021130 <1981936752@qq.com>
2023-05-01 00:38:50 -0400

    creating docs for transactions

---
+++ myapp/models/TransactionItem.js
@@ -0,0 +1,15 @@
+'use strict';
+const mongoose = require( 'mongoose' );
+const Schema = mongoose.Schema;
+const ObjectId = mongoose.Schema.Types.ObjectId;
+
+var toDoItemSchema = Schema( {
+    description: String,
+    amount: Number,
+    category: String,
+    date: Date,
+    userId: { type: ObjectId, ref: 'user' },
+    username: String
+} );
+
+module.exports = mongoose.model( 'transactionItem', transactionItemSchema );
